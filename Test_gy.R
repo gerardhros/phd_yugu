0c7b0cb16bd37e450b2546e7e2ec9391cd537486
Gu <yu.gu@wur.nl>
2023-03-02 12:11:26 +0100

    test-1

--- Test_gy.R
+++ Test_gy.R
@@ -4,3 +4,5 @@
 # to synchronise, step 3: git pull
 
 # really push to gerardhros or yugu0219?
+# sometimes not sure if I push to the repository in your account or the repository in my own account
+
